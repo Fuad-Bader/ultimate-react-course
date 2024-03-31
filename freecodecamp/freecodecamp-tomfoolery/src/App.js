fe754853c3a84deb3c6a33f8145926098b23aad8
Fuad Bader <fbader@kayanhr.com>
2024-03-31 22:22:40 +0300

    made some progress with freecodecamp goofery

---
+++ freecodecamp/freecodecamp-tomfoolery/src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+class ToggleComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visibility: false
+        };
+        // Change code below this line
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+        // Change code above this line
+    }
+    toggleVisibility() {
+        this.setState(state=>({
+            visibility: !state.visibility
+        }));
+    }
+    render() {
+        return (
+            <div>
+                { /* Change code below this line */ }
+                <button onClick={this.handleClick}>Click Me</button>
+                { /* Change code above this line */ }
+                <h1>{this.state.text}</h1>
+            </div>
+        );
+    }
+}
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+        // Change code below this line
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
+        // Change code above this line
+    }
+    // Change code below this line
+    increment(){
+        this.setState(state=>({
+            count: state.count + 1
+        }))
+    }
+
+    decrement(){
+        this.setState(state=>({
+            count: state.count - 1
+        }))
+    }
+
+    reset(){
+        this.setState(state=>({
+            count: 0
+        }))
+    }
+    // Change code above this line
+    render() {
+        return (
+            <div>
+                <button className='inc' onClick={this.increment}>Increment!</button>
+                <button className='dec' onClick={this.decrement}>Decrement!</button>
+                <button className='reset' onClick={this.reset}>Reset</button>
+                <h1>Current Count: {this.state.count}</h1>
+            </div>
+        );
+    }
+}
+export default App;
